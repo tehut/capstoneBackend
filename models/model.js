37ddd438c8834c224222cd0d1c1e0ab5fc2ce022
Tehut Getahun <tehut.m.g@gmail.com>
2017-07-08 17:32:01 -0700

    modified db Schema from animals to invoices

--- models/model.js
+++ models/model.js
@@ -3,19 +3,15 @@ var Schema = mongoose.Schema;
 
 // See http://mongoosejs.com/docs/schematypes.html
 
-var animalSchema = new Schema({
-	name: String,
+var invoiceSchema = new Schema({
+	child: String,
 	// name: {type: String, required: true}, // this version requires this field to exist
 	// name: {type: String, unique: true}, // this version requires this field to be unique in the db
-	age: Number,
+	date_applied: Date,
+	paid: Boolean,
 	tags: [String],
-	description: {
-		weight: Number,
-		color: String
-	},
-	url: String,
 	dateAdded : { type: Date, default: Date.now },
 })
 
 // export 'Animal' model so we can interact with it in other files
-module.exports = mongoose.model('Animal',animalSchema);
+module.exports = mongoose.model('Invoice',invoiceSchema);
