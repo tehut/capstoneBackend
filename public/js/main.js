7d4fccd4da43ee24bdc20b7423f9fc5ea63acf3a
Tehut Getahun <tehut.m.g@gmail.com>
2017-07-25 17:12:28 -0700

    wrote function to parse sessions into invoice lines

--- public/js/main.js
+++ public/js/main.js
@@ -26,7 +26,7 @@ const realmId        = process.env.REALM_ID;
 const username       = process.env.ADMIN_EMAIL;
 const password       = process.env.ADMIN_PASSWORD;
 const tcToken        = process.env.TC_TOKEN;
-const tcURL          = 'https://www.transparentclassroom.com/';
+const tcURL          = 'http://localhost:3000/';
 const qbURL          = 'https://quickbooks.api.intuit.com/v3/company/' + realmId;
 let lastCallDate     = '06-01-2017'
 let join             = Promise.join;
@@ -41,111 +41,11 @@ function Customer(response){
   this.famName = response.fields['child_name.last'];
   this.parents = response.fields['mother/Guardian_name.first']; + ' & ' + response.fields['father/guardian_name.first']
   this.pEmails = response.fields["mother_email"] + ", " + response.fields["father_email"]
-  // this.sGuardianEmail = response.fields['Secondary Guardian_email']
-}
-
-
-// defining the getFullApplications function where the indiv application is called
-// let customerList = [];
-function getFullApplications(newAppIds){
-  let appResponses = [];
-  let customerList = [];
-
-  for (var i = 0; i <newAppIds.length; ++i){
-    // newAppIds.forEach(function(appIdNumber){
-
-    // console.log('inside loop');
-    // console.log(child);
-    var childOptions = {
-      url: tcURL + 'api/v1/online_applications/' + newAppIds[i]+ '.json' ,
-      method: 'GET',
-      headers: {
-        'X-TransparentClassroomToken': tcToken
-      }
-    };
-    appResponses.push(request.getAsync(childOptions))
-    }
-    Promise.all(appResponses)
-      // .each(function(response){
-      //
-      //   // appResponses.each(function(response){
-      //   customerList.push(scrubResponse(response));
-      //   // console.log(response. .body);
-      //   // });
-      //   // for (var i = 0; i <appResponses.length; ++i){
-      //   //   console.log(appResponses[i].Promise.IncomingMessage);
-      //   // console.log( customerList)
-      //   return customerList
-      // })
-      .then(function(customerList){
-        return customerList.map(customer => {
-          return scrubResponse(customer)
-        })
-      }).then((customers)=> {
-        console.log(customers);
-        return customers
-      })
-// console.log(customerList);
-
-  }
-
-  function scrubResponse(response){
-    //
-
-      let bodyObject = JSON.parse(response.body);
-      let thisCustomer = new Customer(bodyObject);
-      // scrubbing output for errors from variation in json keys
-        if (thisCustomer.parents === undefined){
-          thisCustomer.parents = bodyObject.fields['Primary Legal Guardian name:.first'] + ' & ' +bodyObject.fields['Secondary Guardian name:.first']
-        }
-        if (bodyObject.pEmails === undefined) {
-          thisCustomer.pEmails = bodyObject.fields['Primary Guardian_email '] + ' , ' + bodyObject.fields['Secondary Guardian_email']
-        } else if (bodyObject.fields.mother_email.includes(bodyObject.fields.father_email)){
-          thisCustomer.pEmails = bodyObject.fields.mother_email;
-        }
-        console.log("working on element");
-
-      return(thisCustomer);
-  };
-      // customerList.push(thisCustomer);
-      // return new Promise.resolve(customerList)
-      // console.log(customerList);
-      // })
-
-    // Promise.all(customerList)
-
-    // return customerList
-    //
-    // })
-  //   .then((customerList) => {
-  //     console.log(customerList);
-  // //
-  //     console.log('last promise');
-  //     console.log(customerList);
-  //     // return Promise.resolve(customerList)
-  //   })
-  //   .then((customerList) => {
-  //     // console.log('after promise');
-  //   return Promise.resolve(customerList)
-  // })
-  // .then((customerList) => {
-  //   return Promise.resolve(customerList)
-  // })
-  // .then((customerList) => {
-  //   console.log(customerList);
-  // })
-  //   .catch((error) => {
-  //     console.log(error);
-
-
-  // });
-// };
-// console.log(customerList);
-
+};
 
 
 function pullApplications (lastCallDate){
-// I think i'm giong to have to store lastCallDate in the database otherwise it wont persist between scripts
+  // I think i'm giong to have to store lastCallDate in the database otherwise it wont persist between scripts
   // if(!lastCallDate){lastCallDate = Date.}
   const options = {
     url: tcURL + 'api/v1/online_applications.json' + '?created_at=' + lastCallDate,
@@ -154,23 +54,131 @@ function pullApplications (lastCallDate){
       'X-TransparentClassroomToken': tcToken
     }
   };
-
-
   return request.getAsync(options)
 };
 
 function buildAppIdArray (response){
-
-    const newAppIds      = [];
-    let newApps = JSON.parse(response.body);
-    lastCallDate = response.caseless.dict.date
-    for (var app of newApps){
-      newAppIds.push(app.id);
-    }
-    return new Promise.resolve(newAppIds)
+  const newAppIds      = [];
+  let newApps = JSON.parse(response.body);
+  lastCallDate = response.caseless.dict.date
+  for (var app of newApps){
+    newAppIds.push(app.id);
+  }
+  return new Promise.resolve(newAppIds)
 }
 
+// defining the getFullApplications function where the indiv application is called
+// let customerList = [];
+  function scrubResponse(response){
+    let bodyObject = JSON.parse(response.body);
+    let thisCustomer = new Customer(bodyObject);
+    // scrubbing output for errors from variation in json keys
+    if (thisCustomer.parents === undefined){
+      thisCustomer.parents = bodyObject.fields['Primary Legal Guardian name:.first'] + ' & ' +bodyObject.fields['Secondary Guardian name:.first']
+    }
+    if (bodyObject.pEmails === undefined) {
+      thisCustomer.pEmails = bodyObject.fields['Primary Guardian_email '] + ' , ' + bodyObject.fields['Secondary Guardian_email']
+    } else if (bodyObject.fields.mother_email.includes(bodyObject.fields.father_email)){
+      thisCustomer.pEmails = bodyObject.fields.mother_email;
+    }
 
+
+    return(thisCustomer);
+  };
+
+
+  function getFullApplications(newAppIds){
+    let appResponses = [];
+    let customerList = [];
+
+    for (var i = 0; i <newAppIds.length; ++i){
+      var childOptions = {
+        url: tcURL + 'api/v1/online_applications/' + newAppIds[i]+ '.json' ,
+        method: 'GET',
+        headers: {
+          'X-TransparentClassroomToken': tcToken
+        }
+      };
+      appResponses.push(request.getAsync(childOptions))
+      }
+      Promise.all(appResponses)
+
+        .then(function(customerList){
+          return customerList.map(customer => {
+            return scrubResponse(customer)
+          })
+        }).then((customers)=> {
+          let invoiceArray =[];
+          customers.forEach(customer=>{
+            let qbo = new QuickBooks(consumerKey,
+              consumerSecret,
+              accessToken,
+              accessSecret,
+              realmId,
+              true, // use the Sandbox
+              true);
+            var customerQuery = [
+                            {field: 'fetchAll', value: true},
+                            // {field: 'GivenName', value: customer.gName, operator: 'LIKE'},
+                            {field: 'FamilyName', value: customer.famName, operator: 'LIKE'}
+            ]
+            qbo.findCustomers(customerQuery,function(error, response){
+              // TODO: The app currently doesn not return an error when there is no student Found
+              // Amerlia Fu is not a customer but she just returns an empty object==? "QueryResponse": {}
+              if(response.QueryResponse != {}){
+                console.log('this is the response ');
+
+                let sessions = ""
+                 customer.sessions.forEach((customer)=>{
+                   sessions += customer
+                });
+                // console.log(sessions);
+                // var invoiceObject = {
+                //   "Invoice": {
+                //     "Deposit": 0,
+                //     "domain" : "QBO",
+                //     ''
+                //   }
+                // }
+                  if (customer.sessions.length == 2) {
+                  console.log("Summer and Fall Registration & Craft Fees : \" $350 \" ");
+                } else if (customer.sessions.length == 1 && sessions.includes("school years")){
+                  console.log("Fall Registration & Craft Fees : \" $250 \" ");
+                }else if (customer.sessions.length == 1 && sessions.includes("summer")){
+                  console.log("Summer Registration & Craft Fees : \" $200 \" ");
+                }
+            }
+              else {
+                console.log(response.QueryResponse);
+              }
+          },this)
+        })
+      });
+    };
+
+function postCustomer(customerList){
+  console.log("this is postCustomer" +customerList);
+  let qbo = new QuickBooks(consumerKey,
+    consumerSecret,
+    accessToken,
+    accessSecret,
+    realmId,
+    true, // use the Sandbox
+    true); // turn debugging on
+    // console.log("this is customerList " + customerList);
+
+    qbo.findCustomers([
+
+      {field: 'fetchAll', value: true},
+      {field: 'GivenName', value: 'Oren', operator: 'LIKE'},
+      {field: 'FamilyName', value: 'Hess', operator: 'LIKE'}
+    ], function(error, response){
+        // console.log("TG>>> respons in the callback: " + Object.keys(response.QueryResponse.Customer));
+        response.QueryResponse.Customer.forEach(function(customer){
+          // return new Promise
+        });
+      });
+  };
 
 
 pullApplications(lastCallDate)
@@ -178,123 +186,17 @@ pullApplications(lastCallDate)
   .then(getFullApplications)
   // .then(postCustomer)
 
-// postCustomer()
 
-//
-// app.get('/requestToken', function(req, res) {
-//   var postBody = {
-//     url: QuickBooks.REQUEST_TOKEN_URL,
-//     oauth: {
-//       callback:        'http://localhost:' + port + '/callback/',
-//       consumer_key:    consumerKey,
-//       consumer_secret: consumerSecret
-//     }
-//   }
-//   request.post(postBody, function (e, r, data) {
-//     var requestToken = qs.parse(data)
-//     req.session.oauth_token_secret = requestToken.oauth_token_secret
-//
-//     res.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token)
-//   })
-//   console.log(data);
-//   console.log(postBody);
-//   console.log(requestToken);
-//
+// Another approach to promises.map
 
-// let qbo = new QuickBooks(consumerKey,
-//                      consumerSecret,
-//                      accessToken,
-//                      accessSecret,
-//                      realmId,
-//                      true, // use the Sandbox
-//                      true); // turn debugging on
-//
-// // test out account access
-// qbo.findCustomers(function(_,QueryResponse) {
-// // var options {
-// //
-// // }
-//   console.log(Object.keys(QueryResponse));
-//   console.log(QueryResponse.QueryResponse.Customer[0].PrimaryEmailAddr);
-//   // accounts.findCustomers.Account.forEach(function(account) {
-//   //   console.log(account.Name)
-//   // })
-// })
-// // }
-// })
-// })
-// res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
-// })
-
-
-
-// createCustomer(newAppIds)
-// function postCustomer(customerList){
-  // console.log(customerList);
-// app.get('/requestToken', function(req, res) {
-//   var postBody = {
-//     url: QuickBooks.REQUEST_TOKEN_URL,
-//     oauth: {
-//       callback:        'http://localhost:' + port + '/callback/',
-//       consumer_key:    consumerKey,
-//       consumer_secret: consumerSecret
-//     }
-//   }
-//   request.post(postBody, function (e, r, data) {
-//     var requestToken = qs.parse(data)
-//     req.session.oauth_token_secret = requestToken.oauth_token_secret
-//
-//     res.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token)
-//   })
-//   // console.log(data);
-//   // console.log(postBody);
-//   // console.log(requestToken);
-// })
-
-let qbo = new QuickBooks(consumerKey,
-                     consumerSecret,
-                     accessToken,
-                     accessSecret,
-                     realmId,
-                     true, // use the Sandbox
-                     true); // turn debugging on
-
-// test out account access
-var options = {
-  "GivenName":"h",
-  "FamilyName":"James",
-  'BillAddr': {
-    "Line1": "Jim and James"
-  },
-  'PrimaryEmailAddr': {
-    "Address":
-      "Jenny@a.com, mark@sdfsdmark.com"
-
-  }
-};
-
-//   qbo.createCustomer(options, function(error,body){
-// //
-// console.log("this is body" + Object.keys(body))
-// console.log("this is error" + Object.keys(error));
-// // console.log(Customer);
-//
-//
-//   console.log(Object.keys(QueryResponse));
-//   console.log(QueryResponse.QueryResponse.Customer[0].PrimaryEmailAddr);
-//   accounts.findCustomers.Account.forEach(function(account) {
-//     console.log(account.Name)
-//   })
-// });
-
-// };
-
-
-
-
-// pullApplications(lastCallDate)
-  // .then(getFullApplications(newAppIds))
-  // .then(postCustomer(customerList));
-// // })
-// // res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
-// //
+  // .each(function(response){
+  //
+  //   // appResponses.each(function(response){
+  //   customerList.push(scrubResponse(response));
+  //   // console.log(response. .body);
+  //   // });
+  //   // for (var i = 0; i <appResponses.length; ++i){
+  //   //   console.log(appResponses[i].Promise.IncomingMessage);
+  //   // console.log( customerList)
+  //   return customerList
+  // })
